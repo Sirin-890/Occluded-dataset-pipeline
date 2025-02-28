c809b036755b3bafca8fa60c842c57297a95421f
sirin changulani <bappa123@sirins-MacBook-Air.local>
2025-03-01 02:15:36 +0530

    pipline ready for glass and mask

--- pipeline.py
+++ pipeline.py
@@ -3,10 +3,9 @@ from face_detection import face_detection_crop
 from occlusion_blackbox.sunglasses import sunglass_occlusion
 from occlusion_blackbox.percent_ten import percent_ten_blackbox_single
 from loguru import logger
-
-
-
-def black(input_dir,output_dir_black,percent):
+import argparse
+# 3 different function for 3 different task 
+def black(input_dir,output_dir_black,percent): # isme percent mein hi  pass kardenge 10 ya 20 ya 40
     for root, _, files in os.walk(input_dir):
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg')):
@@ -39,10 +38,50 @@ def glasses(input_dir,output_dir_glasses,sunglass_image_path):
                 glasses_image=sunglass_occlusion(croped,x,y,w,h,sunglass_image_path)
                 cv2.imwrite(output_path, glasses_image)
                 logger.info(f"Processed and saved: {output_path}")
-def mask(input_dir,output_dir_mask):
+def mask(input_dir,output_dir_mask): # isme bcs phele se likha code change karna hai tu uska function mein thodi samasyaa hai
     pass
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+    description="to make occluded dataset "
+    )
 
+    parser.add_argument(
+        "--path",
+        type=str,
+        default="",
+        help="input directory of images",
+    )
+    parser.add_argument(
+        "--output_blackbox_path",
+        type=str,
+        default="",
+        help="output black box  directory of images",
+    )
+    parser.add_argument(
+        "--output_glasses_path",
+        type=str,
+        default="",
+        help="output glasses directory of images",
+    )
+    parser.add_argument(
+        "--glass_image_path",
+        type=str,
+        default="",
+        help="input of glass  images",
+    )
+    parser.add_argument(
+        "--percent",
+        type=str,
+        default="",
+        help="percent black box occlusion ",
+    )
+    args = parser.parse_args()
+    black(args.path,args.output_blackbox_path,args.percent)
+    glasses(args.path,args.output_glasses_path,args.glass_image_path)
+
+        
+        
 
 
 
